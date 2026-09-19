9dfd024a9963ace466f6fecdc03311657958ae97
Dave <daveb.merino@gmail.com>
2026-09-19 15:31:15 +0800

    salaryschedule

--- src/modules/salaryschedule/salarySchedule.routes.js
+++ src/modules/salaryschedule/salarySchedule.routes.js
@@ -4,6 +4,9 @@ const validateRequest = require("../../middleware/validateRequest.middleware");
 const salaryScheduleController = require("./salarySchedule.controller");
 
 const createSalaryScheduleValidator = require("./validators/createSalarySchedule.validator");
+const updateExpenseValidator = require("../expenses/validators/updateExpense.validator");
+const deleteExpenseValidator = require("../expenses/validators/deleteExpense.validator");
+const getExpensesValidator = require("../expenses/validators/getExpense.validator");
 
 const salaryScheduleRoute = express.Router();
 
@@ -14,6 +17,27 @@ salaryScheduleRoute.post(
   createSalaryScheduleValidator,
   validateRequest,
   salaryScheduleController.handleCreateSalarySchedule,
+);
+
+salaryScheduleRoute.patch(
+  "/salary-date",
+  updateExpenseValidator,
+  validateRequest,
+  salaryScheduleController.handleUpdateSalarySchedule,
+);
+
+salaryScheduleRoute.delete(
+  "/salary-date",
+  deleteExpenseValidator,
+  validateRequest,
+  salaryScheduleController.handleDeleteSalarySchedule,
+);
+
+salaryScheduleRoute.get(
+  "/salary-date",
+  getExpensesValidator,
+  validateRequest,
+  salaryScheduleController.handleGetSalarySchedule,
 );
 
 module.exports = salaryScheduleRoute;
